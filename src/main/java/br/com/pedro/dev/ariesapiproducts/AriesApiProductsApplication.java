c58a8f09e19eebf3fa8080b36204e32b09451a22
Pedro Silva <pedro.silva.dev.online@gmail.com>
2023-08-14 18:08:37 -0300

    Primeiro commit

---
+++ src/main/java/br/com/pedro/dev/ariesapiproducts/AriesApiProductsApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedro.dev.ariesapiproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AriesApiProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AriesApiProductsApplication.class, args);
+	}
+
+}
